9d393cf929d614ab2ed5e9287785caeb8bfd8d50
yulin.fyl <yulin.fyl@alibaba-inc.com>
2025-06-30 17:48:59 +0800

    chore: publish

--- src/data/state.rs
+++ src/data/state.rs
@@ -1,7 +1,7 @@
 use crate::ui::OrderBy;
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
-use log::error;
+// use log::error;
 
 #[derive(Debug, Clone, Default)]
 pub struct Metadata {
